417f8ca23e4d159381721620afddbf74eaca532c
kavindaperera <perera.kavinda52@gmail.com>
2022-03-05 19:18:18 +0530

    Add `getAgreement` method - `AgreementRepository `

--- src/main/java/AgreementRepository.java
+++ src/main/java/AgreementRepository.java
@@ -5,6 +5,7 @@ import org.hyperledger.fabric.contract.annotation.Contract;
 import org.hyperledger.fabric.contract.annotation.Default;
 import org.hyperledger.fabric.contract.annotation.Info;
 import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeException;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 
 @Contract(name = "Agreements", info = @Info(title = "Agreements contract", description = "A java chaincode demo", version = "0.0.1-SNAPSHOT"))
@@ -26,6 +27,23 @@ public final class AgreementRepository implements ContractInterface {
         String agreementState = genson.serialize(agreement);
         stub.putStringState("ARG001", agreementState);
     }
+
+    @Transaction()
+    public Agreement getAgreement(final Context ctx, String key) {
+        ChaincodeStub stub = ctx.getStub();
+        String agreementState = stub.getStringState(key);
+
+        if (agreementState.isEmpty()){
+            String errorMessage = String.format("Agreement %s does not exist", key);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, "Agreement not found");
+        }
+
+        Agreement agreement = genson.deserialize(agreementState, Agreement.class);
+
+        return agreement;
+    }
+
 
 
 }
