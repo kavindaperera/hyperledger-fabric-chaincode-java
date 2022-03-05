4837163b061d8fac782a01ffd33fc9e7bcae8c74
kavindaperera <perera.kavinda52@gmail.com>
2022-03-05 19:14:31 +0530

    Add `initLedger` method - `AgreementRepository `

--- src/main/java/AgreementRepository.java
+++ src/main/java/AgreementRepository.java
@@ -1,11 +1,31 @@
 import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
-import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeStub;
 
 @Contract(name = "Agreements", info = @Info(title = "Agreements contract", description = "A java chaincode demo", version = "0.0.1-SNAPSHOT"))
 @Default
 public final class AgreementRepository implements ContractInterface {
 
     private final Genson genson = new Genson();
+
+    /**This method will be call only the first time that the chaincode is deployed,
+     * and will create an agreement to test that the query is working properly.
+     * In a real use case, this action could be avoided.
+     * @param ctx
+     */
+    @Transaction()
+    public void initLedger(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+        Agreement agreement = new Agreement("MyCompany", "OtherCompany", "open");
+
+        String agreementState = genson.serialize(agreement);
+        stub.putStringState("ARG001", agreementState);
+    }
+
 
 }
